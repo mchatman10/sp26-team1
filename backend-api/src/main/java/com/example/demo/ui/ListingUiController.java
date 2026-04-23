471cdefa6f2d0c4adc11a657c86c22b1cf55a50b
Zachary Fouts <zmfouts@uncg.edu>
2026-04-22 20:05:48 -0400

    began base of Ui Controllers and Coaching Post ftlh

--- backend-api/src/main/java/com/example/demo/ui/ListingUiController.java
+++ backend-api/src/main/java/com/example/demo/ui/ListingUiController.java
@@ -1,5 +1,44 @@
 package com.example.demo.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Listing;
+import com.example.demo.service.ListingService;
+import java.util.List;
+
+@Controller
+@RequestMapping("/listings")
 public class ListingUiController {
-    
+    @Autowired
+    private ListingService service;
+
+    @GetMapping
+    public List<Listing> getAll() {
+        return service.getAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Listing> getListingById(@PathVariable long id)
+    {
+        Listing listing = service.getListingById(id);
+        if (listing != null) 
+            {
+            return ResponseEntity.ok(listing);
+            }
+        else
+            {
+            return ResponseEntity.notFound().build();
+            }  
+    }
+
+    @PostMapping
+    public Listing create(@RequestBody Listing listing) {
+        return service.save(listing);
+    }
 }
--- backend-api/src/main/java/com/example/demo/ui/ReviewUiController.java
+++ backend-api/src/main/java/com/example/demo/ui/ReviewUiController.java
@@ -1,5 +1,20 @@
 package com.example.demo.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.Review;
+import com.example.demo.service.ReviewService;
+
+@Controller
+@RequestMapping("/reviews")
 public class ReviewUiController {
-    
+
+    @Autowired
+    private ReviewService service;
+
+    @PostMapping
+    public Review create(@RequestParam Long customerId, @RequestParam Long listingId, @RequestBody Review review) {
+        return service.create(customerId, listingId, review);
+    }
 }
